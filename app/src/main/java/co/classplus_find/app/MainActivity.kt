36f13ebaa7f88ffce9d715557a6d84d6ce4b813f
ABRAR AHMAD RAZA <razaahmad610@gmail.com>
2021-06-11 22:39:02 +0530

    logout added

--- app/src/main/java/co/classplus_find/app/MainActivity.kt
+++ app/src/main/java/co/classplus_find/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -34,19 +35,32 @@ class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
         setupData()
         setupUi()
-        setupListeners()
     }
 
-    private fun setupListeners(){
-        binding.signout.setOnClickListener{
-            Firebase.auth.signOut()
-            val intent = Intent(this, ChooseUserActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.logout_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+            R.id.logout -> {
+                Firebase.auth.signOut()
+                val intent = Intent(this, ChooseUserActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     private fun setupUi(){
         val fragList: ArrayList<Fragment> = ArrayList()
